1bd691b3510754c6d67f0a46a6bac94fdd93b7fe
therealazimbek <aaazim04012002@gmail.com>
2023-04-05 11:19:53 +0600

    first commit

---
+++ src/main/java/com/therealazimbek/spring/eventmasterapp/EventmasterAppApplication.java
@@ -0,0 +1,13 @@
+package com.therealazimbek.spring.eventmasterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmasterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventmasterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/therealazimbek/spring/eventmasterapp/EventmasterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therealazimbek.spring.eventmasterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmasterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
