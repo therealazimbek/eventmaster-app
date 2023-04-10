b811ee4f13d8e16399f4c18742370abefcc71dfb
therealazimbek <aaazim04012002@gmail.com>
2023-04-10 18:04:08 +0600

    user repo

---
+++ src/main/java/com/therealazimbek/spring/eventmasterapp/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.therealazimbek.spring.eventmasterapp.repositories;
+
+import com.therealazimbek.spring.eventmasterapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    List<User> findByUsername(String username);
+}
