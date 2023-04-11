360511eb0555b781eab28de334eb3b9d2dc52af9
therealazimbek <aaazim04012002@gmail.com>
2023-04-11 21:22:47 +0600

    models upd

---
+++ src/main/java/com/therealazimbek/spring/eventmasterapp/models/TaskCategory.java
@@ -0,0 +1,27 @@
+package com.therealazimbek.spring.eventmasterapp.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class TaskCategory {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank(message="Title is required and it should be unique")
+    @Size(min=5, message="Title must be at least 5 characters long")
+    private String title;
+
+    private String details;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.PERSIST)
+    private List<Task> tasks = new ArrayList<>();
+}
